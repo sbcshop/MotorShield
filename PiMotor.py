c5a855bf804f722383151be66f63e0865cda9a8b
sbcshop <ian.blundell@sb-components.co.uk>
2017-02-26 19:20:04 +0000

    Support for speed control
    Added support for speed control

--- PiMotor.py
+++ PiMotor.py
@@ -22,10 +22,10 @@ class Motor:
             the motor is connected.
     config = int defining which pins control "forward" and "backward" movement.
     '''
-    motorpins = {"MOTOR1":{"config":{1:{"e":32,"f":24,"r":26},2:{"e":32,"f":26,"r":24}},"arrow":1},
-                 "MOTOR2":{"config":{1:{"e":19,"f":21,"r":23},2:{"e":19,"f":23,"r":21}}, "arrow":2},
-                 "MOTOR3":{"config":{1:{"e":22,"f":16,"r":18},2:{"e":22,"f":18,"r":16}}, "arrow":3},
-                 "MOTOR4":{"config":{1:{"e":11,"f":15,"r":13},2:{"e":11,"f":13,"r":15}},"arrow":4}}
+    motorpins = {"MOTOR4":{"config":{1:{"e":32,"f":24,"r":26},2:{"e":32,"f":26,"r":24}},"arrow":1},
+                 "MOTOR3":{"config":{1:{"e":19,"f":21,"r":23},2:{"e":19,"f":23,"r":21}}, "arrow":2},
+                 "MOTOR2":{"config":{1:{"e":22,"f":16,"r":18},2:{"e":22,"f":18,"r":16}}, "arrow":3},
+                 "MOTOR1":{"config":{1:{"e":11,"f":15,"r":13},2:{"e":11,"f":13,"r":15}},"arrow":4}}
     
     def __init__(self, motor, config):
         self.testMode = False
@@ -34,6 +34,8 @@ class Motor:
         GPIO.setup(self.pins['e'],GPIO.OUT)
         GPIO.setup(self.pins['f'],GPIO.OUT)
         GPIO.setup(self.pins['r'],GPIO.OUT)
+        self.PWM = GPIO.PWM(self.pins['e'], 50)  # 50Hz frequency
+        self.PWM.start(0)
         GPIO.output(self.pins['e'],GPIO.HIGH)
         GPIO.output(self.pins['f'],GPIO.LOW)
         GPIO.output(self.pins['r'],GPIO.LOW)
@@ -48,23 +50,33 @@ class Motor:
         '''
         self.testMode = state
 
-    def forward(self):
+    def forward(self, speed):
         ''' Starts the motor turning in its configured "forward" direction.
-'''
+
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed
+        '''    
         print("Forward")
         if self.testMode:
             self.arrow.on()
         else:
+            self.PWM.ChangeDutyCycle(speed)
             GPIO.output(self.pins['f'],GPIO.HIGH)
             GPIO.output(self.pins['r'],GPIO.LOW)
 
-    def reverse(self):
+    def reverse(self,speed):
         ''' Starts the motor turning in its configured "reverse" direction.
+
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed
      '''
         print("Reverse")
         if self.testMode:
             self.arrow.off()
         else:
+            self.PWM.ChangeDutyCycle(speed)
             GPIO.output(self.pins['f'],GPIO.LOW)
             GPIO.output(self.pins['r'],GPIO.HIGH)
 
@@ -73,8 +85,13 @@ class Motor:
      '''
         print("Stop")
         self.arrow.off()
+        self.PWM.ChangeDutyCycle(0)
         GPIO.output(self.pins['f'],GPIO.LOW)
         GPIO.output(self.pins['r'],GPIO.LOW)
+
+    def speed(self):
+        ''' Control Speed of Motor,
+     '''
 
 class LinkedMotors:
     ''' Links 2 or more motors together as a set.
@@ -92,17 +109,25 @@ class LinkedMotors:
             print(i.pins)
             self.motor.append(i)
 
-    def forward(self):
+    def forward(self,speed):
         ''' Starts the motor turning in its configured "forward" direction.
-     '''
-        for i in range(len(self.motor)):
-            self.motor[i].forward()
 
-    def reverse(self):
-        ''' Starts the motor turning in its configured "reverse" direction.
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed 
      '''
         for i in range(len(self.motor)):
-            self.motor[i].reverse()
+            self.motor[i].forward(speed)
+
+    def reverse(self,speed):
+        ''' Starts the motor turning in its configured "reverse" direction.
+
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed
+     '''
+        for i in range(len(self.motor)):
+            self.motor[i].reverse(speed)
 
     def stop(self):
         ''' Stops power to the motor,
@@ -144,7 +169,8 @@ class Sensor:
         self.lastRead = measure
         if self.boundary > measure:
             print("Boundary breached")
-            print(self.boundary, measure)
+            print(self.boundary)
+            print(measure)
             self.Triggered = True
         else:
             self.Triggered = False
@@ -190,7 +216,3 @@ class Arrow():
 
     def off(self):
         GPIO.output(self.pin,GPIO.LOW)
-
-    
-
-    
