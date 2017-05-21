95eba2e524fe34f1d884078c04123c6140ecaab9
Jille Timmermans <jille@quis.cx>
2017-05-21 14:08:43 +0100

    Let PiMotorTest also test the reverse

--- PiMotorTest.py
+++ PiMotorTest.py
@@ -13,7 +13,9 @@ def run():
         for config in range(1,3):
             print(motor + " config: " + str(config))
             m = PiMotor.Motor(motor,config)
-            m.forward()
+            m.forward(50)
+            time.sleep(2)
+            m.reverse(50)
             time.sleep(2)
             m.stop()
 
