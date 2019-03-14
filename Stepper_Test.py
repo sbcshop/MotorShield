7342c15e4283eb7d1694e1e5b4a3c4d6c9145e7d
sbcshop <gajender.singh@sb-components.co.uk>
2019-03-14 14:36:27 +0530

    Update Stepper_Test.py
    keyboard interrupt added

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -1,5 +1,6 @@
 import PiMotor
 import time
+import RPi.GPIO as GPIO
 
 m1 = PiMotor.Stepper("STEPPER1")
 
@@ -9,3 +10,6 @@ while True:
     time.sleep(2)
     m1.backward(0.1,10)
     time.sleep(2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
