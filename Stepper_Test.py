51ef865755fa129bb4354c2d436b650363636a65
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-05-06 12:07:50 +0530

    Update Stepper_Test.py

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -7,8 +7,8 @@ m1 = PiMotor.Stepper("STEPPER1")
 # Rotate Stepper 1 Contiously in forward/backward direction
 try:
     while True:
-        m1.forward(0.1,10)  # Delay and rotations
-        time.sleep(2)
+        m1.forward(0.1,10)  # m1.forward(delay,steps) (0.1,10) 
+        time.sleep(2)       # delay between forward and backward
         m1.backward(0.1,10)
         time.sleep(2)
 except KeyboardInterrupt:
